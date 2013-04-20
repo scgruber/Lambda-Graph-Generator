678e6b3750770a02d885c70d2b166b8c3d732821
Sam Gruber <grubermensch@gmail.com>
2013-04-20 10:27:17 -0400

    transform matrix reset; spherical -> cartesian

--- util.js
+++ util.js
@@ -1,3 +1,17 @@
+/* Test if a string is alphabetic */
 function isAlpha(str) {
   return !/[^a-zA-Z]/.test(str);
+}
+
+/* Reset the drawing transformation matrix */
+function resetTransform(ctx) {
+  ctx.setTransform(1, 0, 0, 1, 0, 0);
+}
+
+/* Convert spherical coords to cartesian */
+function sphericalToCartesian(r, theta) {
+  return {
+    x: r * Math.cos(theta),
+    y: r * Math.sin(theta)
+  }
 }
