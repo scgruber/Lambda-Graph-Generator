2b31b74492a894eccc3bbf3f0525f516ba87344a
Sam Gruber <grubermensch@gmail.com>
2013-04-21 11:15:29 -0400

    added euclidean distance function

--- util.js
+++ util.js
@@ -21,3 +21,8 @@ function sphericalToCartesian(r, theta) {
     y: r * Math.sin(theta)
   }
 }
+
+/* Calculate euclidean distance */
+function dist(x1, y1, x2, y2) {
+  return Math.sqrt( ((x1-x2)*(x1-x2)) + ((y1-y2)*(y1-y2)));
+}
