c1a766e0b478226ca99afafde53ab51e95382341
Sam Gruber <grubermensch@gmail.com>
2013-04-28 17:17:36 -0400

    add Vec2D object for working with 2D vectors

--- util.js
+++ util.js
@@ -34,3 +34,61 @@ function sphericalToCartesian(r, theta, cx, cy) {
 function dist(x1, y1, x2, y2) {
   return Math.sqrt( ((x1-x2)*(x1-x2)) + ((y1-y2)*(y1-y2)));
 }
+
+/* Calculate nearest point on p1-p2 to x */
+function nearestPoint(p1, p2, x) {
+  var dir = Vec2D.sub(p2, p1);
+  var t = Vec2D.dot(dir, Vec2D.sub(x, p1)) / Vec2D.dot(dir, dir);
+  if (t < 0) {
+    /* Ideal point is before start */
+    return Vec2D.copy(p1);
+  } else if (t > 1) {
+    return Vec2D.copy(p2);
+  } else {
+    return Vec2D.add(p1, Vec2D.mult(t, dir));
+  }
+}
+
+/****************
+ * Vec2D Object *
+ ****************/
+function Vec2D(x, y) {
+  return {
+    x: x,
+    y: y
+  }
+}
+
+Vec2D.prototype.copy = function(v) {
+  return {
+    x: v.x,
+    y: v.y
+  }
+}
+
+/* Arithmetic operators */
+Vec2D.prototype.add = function(v, w) {
+  return {
+    x: v.x + w.x,
+    y: v.y + w.y
+  };
+}
+
+Vec2D.prototype.sub = function(v, w) {
+  return {
+    x: v.x - w.x,
+    y: v.y - w.y
+  }
+}
+
+Vec2D.prototype.dot = function(v, w) {
+  return (v.x * w.x) + (v.y * w.y);
+}
+
+Vec2D.prototype.mult = function(s, v) {
+  return {
+    x: s * v.x,
+    y: s * v.y
+  }
+}
+
